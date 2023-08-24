dc284748cc7eedc83acefc7152b2040dea36a05f
clevermacaw <ryan.cv.cooper@outlook.com>
2023-08-24 09:43:26 +0300

    added NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+</script>
+
 <template>
   <div class="bg-gray-300 dark:bg-gray-900 h-screen">
+    <nav-bar />
     <router-view class="flex-grow p-4"></router-view>
   </div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-between p-4 shadow-lg bg-gray-50 dark:bg-gray-800">
+    <div class="flex text-2xl text-gray-600 font-bold">
+      <a
+        href="https://www.mailerlite.com/"
+        class="hover:cursor-pointer text-purple-500 hover:text-purple-800"
+        >MailerLite</a
+      >&nbsp;Assessment
+    </div>
+    <div class="flex space-x-6 text-lg">
+      <button>Save</button>
+    </div>
+  </div>
+</template>
